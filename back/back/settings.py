5b27f9b45928bba8be9432a075df45fb31cedb8c
あなたのGitHubユーザー名 <GitHub>
2025-08-24 14:07:44 +0900

    Add new public Vercel project URL to CORS settings

--- back/back/settings.py
+++ back/back/settings.py
@@ -35,7 +35,8 @@ CORS_ALLOWED_ORIGINS = [
     "http://127.0.0.1:3001",
     "https://trumee-h7ygmkzis-taiga1226s-projects.vercel.app",  # 旧URL1
     "https://trumee-3msjm6bnn-taiga1226s-projects.vercel.app",  # 旧URL2
-    "https://trumee-3jps5yj06-taiga1226s-projects.vercel.app",  # 最新URL
+    "https://trumee-3jps5yj06-taiga1226s-projects.vercel.app",  # 旧URL3
+    "https://trumeee-8ygeu8cmc-taiga1226s-projects.vercel.app",  # 新パブリックURL
 ]
 
 CORS_ALLOW_CREDENTIALS = True
