68d087e5be01a841fb106d4fb561bd7822345410
あなたのGitHubユーザー名 <GitHub>
2025-09-13 10:41:25 +0900

    Add migration for merging job posting and plan tier migrations

---
+++ back/core/migrations/0009_merge_0008_jobposting_0008_plan_tier.py
@@ -0,0 +1,12 @@
+from django.db import migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('core', '0008_add_user_plan_tier'),
+        ('core', '0008_jobposting'),
+    ]
+
+    operations = []
+
