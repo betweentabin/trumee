3f3c47ee69d1822416b286791a1793cdce3cb40a
あなたのGitHubユーザー名 <GitHub>
2025-09-15 02:01:39 +0900

    Add merge migration for resume file model

---
+++ back/core/migrations/0010_merge_0008_add_resume_file_model_0009_merge.py
@@ -0,0 +1,12 @@
+from django.db import migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('core', '0008_add_resume_file_model'),
+        ('core', '0009_merge_0008_jobposting_0008_plan_tier'),
+    ]
+
+    operations = []
+
